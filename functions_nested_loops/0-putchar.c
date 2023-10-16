1a074a5771b0e27e0255b21c4d5d88195f06c110
ninoshkaxv <perez.ninoshka31@gmail.com>
2023-10-16 17:27:40 -0400

    printing putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
+#include "holberton.h"
 
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	printf("_putchar\n");
-	(return) 0;
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
 }
