9ddc8f4925038a043384e9e1fefd80274fd12555
ninoshkaxv <perez.ninoshka31@gmail.com>
2023-10-17 10:27:28 -0400

    print alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,28 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print the alphabet in lowercase
+ * print_alphabet - Entry point
+ * Return:0
  */
+
 void print_alphabet(void)
 {
-    char c;
+	char c = 'a';
 
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        putchar(c);
-    }
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
 
-    putchar('\n');
+	}
+	_putchar('\n');
+
 }
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
-
