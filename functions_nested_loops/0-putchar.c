45a3558d64e4a1316fd23d8fd4932018d862700b
ninoshkaxv <perez.ninoshka31@gmail.com>
2023-10-17 09:33:36 -0400

    print putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,22 +1,6 @@
-#include <studio>
+#include <studio.h>
 
-/**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
-int main(void)
-{
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-
-	return (0);
+int main() {
+	printf("_putchar\n");
+	return 0;
 }
