e5fef565e51618963941041976fa745e60ac8243
ninoshkaxv <perez.ninoshka31@gmail.com>
2023-10-19 09:35:49 -0400

    print 0 to 9

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * prints_number - prints the numbers, from 0 to 9,
+ * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line
  */
 void print_numbers(void)
 {
-		int i;
+	int i;
 
-		for (i = 0; i < 10; i++)
-		{
-				_putchar(i +'0');
-		}
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
 
-		_putchar('\n');
+	_putchar('\n');
 }
