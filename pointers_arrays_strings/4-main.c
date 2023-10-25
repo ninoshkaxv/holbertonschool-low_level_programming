adf575e366481c628fcc39095c7c7ba531a5bf98
ninoshkaxv <perez.ninoshka31@gmail.com>
2023-10-25 09:40:26 -0400

    rev array

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,26 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_rev - prints a string in reverse, followed by a new line
- * @s: string to be printed
+ * print_array - print an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
  */
-void print_rev(char *s)
+void print_array(int *a, int n)
 {
-	int i, j, len;
+    int i;
 
-	i = 0;
+    i = 0;
+    while (i < n)
+    {
+        if (i != 0)
+        {
+            printf(", ");
+        }
+        printf("%d", a[i]);
+        i++;
+    }
+    printf("\n");
+}
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
-	len = i;
-
-	for (j = len - 1; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
-
-	_putchar('\n');
+    print_array(a, sizeof(a) / sizeof(int));
+    reverse_array(a, sizeof(a) / sizeof(int));
+    print_array(a, sizeof(a) / sizeof(int));
+    return (0);
 }
