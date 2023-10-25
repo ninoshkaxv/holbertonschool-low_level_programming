cb6c9b46a821f053e97b9ec35dd2354a090715ac
ninoshkaxv <perez.ninoshka31@gmail.com>
2023-10-25 10:37:10 -0400

    string toupper

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -1,28 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rev_string - reverses a string
- * @s: string to be reversed
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void rev_string(char *s)
+int main(void)
 {
-	char tmp;
-	int i, len, len1;
+    char str[] = "Look up!\n";
+    char *ptr;
 
-	len = 0;
-	len1 = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	len1 = len - 1;
-
-	for (i = 0; i < len / 2; i++)
-	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
-	}
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
 }
