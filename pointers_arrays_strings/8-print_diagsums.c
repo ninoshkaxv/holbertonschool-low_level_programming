a64b5716e8bf87f91c4279ac35d9d6615d10ce8a
ninoshkaxv <perez.ninoshka31@gmail.com>
2023-10-26 11:01:31 -0400

    print sum of two diagonals of square matrix int

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,23 @@
 #include "main.h"
 
 /**
- * print_chessboard - Prints a chessboard.
- * @a: The 2D array representing the chessboard.
- *
- * This function takes a 2D array representing a chessboard and prints it.
- * Each element of the array is a character representing a chess piece or
- * an empty space.
- *
- * @a: The 2D array to print.
+ * print_diagsums - print the sum of the two diagonals
+ * of a square matrix of integers
+ * @a: square matrix of which print the sum of diagonals
+ * @size: size of the matrix
  */
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-    int i, j;
+	int i;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-            _putchar(a[i][j]);
-        }
-        _putchar('\n');
-    }
+	unsigned int sum, sum1;
+
+	sum = 0;
+	sum1 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum += a[(size * i) + i];
+		sum1 += a[(size * (i + 1)) - (i + 1)];
+	}
 }
