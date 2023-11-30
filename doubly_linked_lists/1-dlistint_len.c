cac6a6e318f9bc1403de794c0482e0a8e42210c6
ninoshkaxv <perez.ninoshka31@gmail.com>
2023-11-30 16:56:14 -0400

    return the number of elements

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - get the number of elements in
+ * a double linked list
+ *
+ * @h: pointer to the  head of the list
+ * Return: the number of nodes in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
