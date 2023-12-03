67a25378400ae252ebb866788fb1f582072b4f02
ninoshkaxv <perez.ninoshka31@gmail.com>
2023-12-03 19:38:59 -0400

    amending code in 4-free_dlistint.c

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - it frees a dlistint_t list
+ *
+ * @head: the head of the list
+ * Return: no return
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
+}
