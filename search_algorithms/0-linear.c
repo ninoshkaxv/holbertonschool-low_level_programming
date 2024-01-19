a8eb3ef879f5ff219b990f6dc9df6af13f3f2027
Ninoshka Perez <perez.ninoshka31@gmail.com>
2024-01-18 20:12:33 -0400

    Fix initial loop declaration

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,7 +2,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches value in array using Linear search algorithm
+ * linear_search - Searches for a value in an array using Linear search algorithm
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in the array
  * @value: Value to search for
@@ -11,16 +11,18 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
+	
 	if (array == NULL)
 		return (-1);
-
-	for (size_t i = 0; i < size; i++)
+	
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-
+		
 		if (array[i] == value)
 			return (i);
-	}
-
+    }
+	
 	return (-1);
 }
