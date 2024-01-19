33f781529ee43562cc60bcf1be6b16b55ac12c31
Ninoshka Perez <perez.ninoshka31@gmail.com>
2024-01-18 20:17:20 -0400

    Fix betty trailing whitespace

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,7 +2,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for a value in an array using Linear search algorithm
+ * linear_search - Searches value in array using Linear search algorithm
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in the array
  * @value: Value to search for
@@ -12,17 +12,17 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
-	
+
 	if (array == NULL)
 		return (-1);
-	
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		
+
 		if (array[i] == value)
 			return (i);
-    }
-	
+	}
+
 	return (-1);
 }
